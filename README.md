1a42ccbb0d3e6373ee8431edb1c192e22abeb492
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-03-06 19:29:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cookie-consent---scrimba-learning-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9mp4u8)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+
+
+
+setTimeout(function(){
+  console.log("modal opened")
+}, 1500)
+
+
+
+
+
+
+
+
