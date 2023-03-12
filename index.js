fa3833fe6f81c0fab026039e71f4618a9fa92e1b
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-03-12 19:02:20 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 // Import stylesheets
 import './style.css';
 
-const modal = document.getElementById("modal")
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
+
 
 
 setTimeout(function(){
   modal.style.display = "inline"
 }, 1500)
+
+modalCloseBtn.addEventListener("click", function() {
+  modal.style.display = "none"
+}
+)
+
+consentForm.addEventListener("submit", function(event) {
+  event.preventDefault()
+  console.log ("form submitted")
+  modalText.innerHTML = `
+  <div class="modal-inner-loading">
+      <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_Wezc{transform-origin:center;animation:spinner_Oiah .75s step-end infinite}@keyframes spinner_Oiah{8.3%{transform:rotate(30deg)}16.6%{transform:rotate(60deg)}25%{transform:rotate(90deg)}33.3%{transform:rotate(120deg)}41.6%{transform:rotate(150deg)}50%{transform:rotate(180deg)}58.3%{transform:rotate(210deg)}66.6%{transform:rotate(240deg)}75%{transform:rotate(270deg)}83.3%{transform:rotate(300deg)}91.6%{transform:rotate(330deg)}100%{transform:rotate(360deg)}}</style><g class="spinner_Wezc"><circle cx="12" cy="2.5" r="1.5" opacity=".14"/><circle cx="16.75" cy="3.77" r="1.5" opacity=".29"/><circle cx="20.23" cy="7.25" r="1.5" opacity=".43"/><circle cx="21.50" cy="12.00" r="1.5" opacity=".57"/><circle cx="20.23" cy="16.75" r="1.5" opacity=".71"/><circle cx="16.75" cy="20.23" r="1.5" opacity=".86"/><circle cx="12" cy="21.5" r="1.5"/></g></svg>
+    <p id="uploadText">
+      Uploading your data to the dark web...
+    </p>
+  </div>`
+}
+)
+
+
+
 
 
 
